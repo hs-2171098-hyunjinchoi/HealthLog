6ac539ffcc4667de1aad5d8999a7f2cd723f8dc5
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-06-19 00:45:02 +0900

    feat: 홈 화면 하단에 식단 분석 메뉴, 운동 분석 메뉴 추가

--- HealthLog/MainViewController.swift
+++ HealthLog/MainViewController.swift
@@ -15,18 +15,18 @@ class MainViewController: UIViewController {
     // MARK: - Setup Methods
     private func setupTabBar() {
         let alarmViewController = UINavigationController(rootViewController: AlarmViewController())
-//        let dietViewController = UINavigationController(rootViewController: DietViewController())
+        let dietAnalysisViewController = UINavigationController(rootViewController: DietAnalysisViewController())
         let dashboardViewController = UINavigationController(rootViewController: DashboardViewController())
-//        let exerciseViewController = UINavigationController(rootViewController: ExerciseViewController())
+        let exerciseAnalysisViewController = UINavigationController(rootViewController: ExerciseAnalysisViewController())
         let calendarViewController = UINavigationController(rootViewController: CalendarViewController())
         
         alarmViewController.tabBarItem = UITabBarItem(title: "Alarm", image: UIImage(systemName: "alarm"), tag: 0)
-//        dietViewController.tabBarItem = UITabBarItem(title: "Diet", image: UIImage(systemName: "fork.knife"), tag: 1)
+        dietAnalysisViewController.tabBarItem = UITabBarItem(title: "Diet", image: UIImage(systemName: "fork.knife"), tag: 1)
         dashboardViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 2)
-//        exerciseViewController.tabBarItem = UITabBarItem(title: "Exercise", image: UIImage(systemName: "figure.walk"), tag: 3)
+        exerciseAnalysisViewController.tabBarItem = UITabBarItem(title: "Exercise", image: UIImage(systemName: "figure.walk"), tag: 3)
         calendarViewController.tabBarItem = UITabBarItem(title: "Calendar", image: UIImage(systemName: "calendar"), tag: 4)
         
-        tabBar.viewControllers = [alarmViewController, dashboardViewController,  calendarViewController]
+        tabBar.viewControllers = [alarmViewController,dietAnalysisViewController,  dashboardViewController, exerciseAnalysisViewController, calendarViewController]
         
         addChild(tabBar)
         view.addSubview(tabBar.view)
